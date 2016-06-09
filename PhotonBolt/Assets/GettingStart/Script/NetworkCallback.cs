b2e0bec112a65a9f40e03002b933e378a422f69b
tintatoi <tintatoi@abc.com>
2016-06-09 18:46:19 +0700

    + commit Photon Getting start

---
+++ PhotonBolt/Assets/GettingStart/Script/NetworkCallback.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : Bolt.GlobalEventListener
+{
+    public override void SceneLoadLocalDone(string map)
+    {
+        // randomize a position
+        var pos = new Vector3(Random.Range(-16, 16), 10, Random.Range(-16, 16));
+
+        // instantiate cube
+        BoltNetwork.Instantiate(BoltPrefabs.Cube, pos, Quaternion.identity);
+    }
+}
