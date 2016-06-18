c861839cded9389f21acb5c4e2592914b43c73e6
tintatoi <tintatoi@abc.com>
2016-06-18 13:32:58 +0700

    * Tutorial Character Animator Controller

--- PhotonBolt/Assets/GettingStart/Script/Menu.cs
+++ PhotonBolt/Assets/GettingStart/Script/Menu.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 public class Menu : Bolt.GlobalEventListener
 {
-    string Endpoint;
-    string Scene;
+    string Endpoint = "127.0.0.1:27000";
+    string Scene = "Tutorial2";
     void OnGUI()
     {
         //Update EndPoint
-        Endpoint = GUI.TextField(new Rect(0, 0, Screen.width, 50), "127.0.0.1:27000");
+        Endpoint = GUI.TextField(new Rect(0, 0, Screen.width, 50), Endpoint);
 
-        Scene = GUI.TextField(new Rect(0, 60, Screen.width, 50), "Tutorial1");
+        Scene = GUI.TextField(new Rect(0, 60, Screen.width, 50), Scene);
 
         GUILayout.BeginArea(new Rect(10, 130, Screen.width - 20, Screen.height - 20));
 
